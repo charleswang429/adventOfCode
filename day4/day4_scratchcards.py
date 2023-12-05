79b9f6f8bcbdaddc14643f2b390cdc318c4582d0
Charles Wang <charleswang@charless-air.lan>
2023-12-05 16:49:17 -0500

    complete part 2

--- day4/day4_scratchcards.py
+++ day4/day4_scratchcards.py
@@ -19,6 +19,21 @@ def part1_find_points(score_card: List[str]) -> int:
     return total_points
 
 
+def part2_num_of_scratchcards(score_card: List[str]) -> int:
+    card_count = [1] * len(score_card)
+    for card_id, card in enumerate(score_card):
+        first, nums_str = card.split(' | ')
+        _, winning_nums_str = first.split(': ')
+        nums, winning_nums = [int(num) for num in nums_str.split()], [int(num) for num in winning_nums_str.split()]
+        matches = len(set(nums).intersection(winning_nums))
+
+        for i in range(matches):
+            card_count[card_id + 1 + i] += card_count[card_id]
+
+    return sum(card_count)
+
+
 if __name__ == '__main__':
     score_card = read_input('day4/input.txt')
     part1_find_points(score_card)
+    part2_num_of_scratchcards(score_card)
